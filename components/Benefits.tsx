18eb02840fb40aa640e9f72944d67ead3fb2ff4c
Basithamid19 <abdulbasitsofi19@gmail.com>
2026-02-15 00:49:45 +0200

    Update Benefits.tsx

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -134,7 +134,7 @@ export const Benefits: React.FC<BenefitsProps> = ({ onNavigate }) => {
     switch(tier) {
       case 'T1': return 'md:min-w-[800px] md:max-w-[800px]'; // XL
       case 'T2': return 'md:min-w-[580px] md:max-w-[580px]'; // L
-      case 'T3': return 'md:min-w-[340px] md:max-w-[340px]'; // S
+      case 'T3': return 'md:min-w-[500px] md:max-w-[500px]'; // S
       default: return 'md:min-w-[400px]';
     }
   };
